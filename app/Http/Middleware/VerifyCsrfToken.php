afaccca3119f089a1435ac028a9765d37916bc00
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2016-08-31 10:41:02 +0200

    Disable CSRF protection for the API

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
@@ -12,6 +13,20 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        //
+
     ];
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (! $request->is('api/*')) {
+            return parent::handle($request, $next);
+        }
+
+        return $next($request);
+    }
 }
