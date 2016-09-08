4e6c4209ba5e0caf840f0ebe85aa4562900b8930
Eduardo Salguero <edusalguero@gmail.com>
2016-09-08 14:50:37 +0200

    Named routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,39 +1,41 @@
 <?php
 
-Route::any('/login', 'Home@login');
+Route::any('/login', ['as' => 'login', 'uses' => 'Home@login']);
 
-Route::get('/401', 'Home@error401');
-Route::get('/404', 'Home@error404');
+Route::get('/401', ['as' => 'error.401', 'uses' => 'Home@error401']);
+Route::get('/404', ['as' => 'error.404', 'uses' => 'Home@error404']);
 
-Route::group(['before' => 'auth'], function()
-{
-    Route::any('/', 'Home@index');
-    Route::get('/stats', 'Home@stats');
-    Route::get('/stats/calendar', 'Home@statsCalendar');
-    Route::any('/sync', 'Home@sync');
-    Route::any('/edit',  [
+Route::group(['before' => 'auth'], function () {
+    Route::any('/', ['as' => 'index', 'uses' => 'Home@index']);
+
+    Route::get('/stats', ['as' => 'stats.index', 'uses' => 'Home@stats']);
+
+    Route::get('/stats/calendar', ['as' => 'stats.calendar', 'uses' => 'Home@statsCalendar']);
+
+    Route::any('/sync', ['as' => 'sync', 'uses' => 'Home@sync']);
+
+    Route::any('/edit', [
         'as' => 'edit.show',
         'uses' => 'Home@edit'
     ]);
 
-    Route::any('/activity/', 'Home@activityAdd');
+    Route::any('/activity/', ['as' => 'activity.add', 'uses' => 'Home@activityAdd']);
 
     Route::any('/activity/{id}', [
         'as' => 'activity.edit',
         'uses' => 'Home@activityEdit'
     ]);
 
+    Route::any('/tag/', ['as' => 'tag.add', 'uses' => 'Home@tagAdd']);
+    Route::any('/tag/{id}', ['as' => 'activity.edit', 'uses' => 'Home@tagEdit']);
 
-    Route::any('/tag/', 'Home@tagAdd');
-    Route::any('/tag/{id}', 'Home@tagEdit');
-    Route::any('/user/', 'Home@userAdd');
-    Route::any('/user/{id}', 'Home@userEdit');
+    Route::any('/user/', ['as' => 'user.add', 'uses' => 'Home@userAdd']);
+    Route::any('/user/{id}', ['as' => 'user.edit', 'uses' => 'Home@userEdit']);
 
     Route::get('client', [
         'as' => 'client.add',
         'uses' => 'ClientController@add'
     ]);
-
     Route::post('client', [
         'uses' => 'ClientController@postAdd'
     ]);
@@ -47,27 +49,26 @@ Route::group(['before' => 'auth'], function()
     ]);
 
 
+    Route::get('/fact-tr/{id}', ['as' => 'fact.tr', 'uses' => 'Home@factTr']);
 
+    Route::get('/dump-sql', ['as' => 'maintenance.sql-dump', 'uses' => 'Home@sqlDownload']);
+    Route::any('/git-update', ['as' => 'maintenance.git-update', 'uses' => 'Home@gitUpdate']);
+    Route::any('/tools-duplicates', ['as' => 'maintenance.duplicates', 'uses' => 'Home@toolsDuplicates']);
 
-    Route::get('/fact-tr/{id}', 'Home@factTr');
-    Route::get('/dump-sql', 'Home@sqlDownload');
-    Route::any('/git-update', 'Home@gitUpdate');
-    Route::any('/tools-duplicates', 'Home@toolsDuplicates');
-    Route::get('/notifications', 'Home@notifications');
-    Route::post('/notifications/{id}/read', 'Home@notificationRead');
+    Route::get('/notifications', ['as' => 'notifications.index', 'uses' => 'Home@notifications']);
+    Route::post('/notifications/{id}/read', ['as' => 'notifications.read', 'uses' => 'Home@notificationRead']);
 });
 
-Route::group(['prefix' => 'api', 'before' => 'auth.api'], function()
-{
-    Route::get('activities', 'Api@getActivities');
-    Route::get('facts', 'Api@getFacts');
-    Route::get('tags', 'Api@getTags');
-    Route::get('facts-tags', 'Api@getFactsTags');
+Route::group(['prefix' => 'api', 'before' => 'auth.api'], function () {
+    Route::get('activities', ['as' => 'api.activities', 'uses' => 'Api@getActivities']);
+    Route::get('facts', ['as' => 'api.facts', 'uses' => 'Api@getFacts']);
+    Route::get('tags', ['as' => 'api.tags', 'uses' => 'Api@getTags']);
+    Route::get('facts-tags', ['as' => 'api.facts.tags', 'uses' => 'Api@getFactsTags']);
 
-    Route::post('activities', 'Api@setActivities');
-    Route::post('facts', 'Api@setFacts');
-    Route::post('tags', 'Api@setTags');
-    Route::post('facts-tags', 'Api@setFactsTags');
+    Route::post('activities', ['uses' => 'Api@setActivities']);
+    Route::post('facts', ['as' => 'notifications.read', 'uses' => 'Api@setFacts']);
+    Route::post('tags', ['as' => 'notifications.read', 'uses' => 'Api@setTags']);
+    Route::post('facts-tags', ['as' => 'notifications.read', 'uses' => 'Api@setFactsTags']);
 
-    Route::delete('facts', 'Api@deleteFacts');
+    Route::delete('facts', ['uses' => 'Api@deleteFacts']);
 });
