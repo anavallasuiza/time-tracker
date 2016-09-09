bc2bd0311fca39605ce6263bacd889e3caac0b51
Eduardo Salguero <edusalguero@gmail.com>
2016-09-09 11:38:22 +0200

    Fix typos

--- resources/views/web/pages/error/500.blade.php
+++ resources/views/web/pages/error/500.blade.php
@@ -3,6 +3,6 @@
     <div class="jumbotron">
         <h1><?php echo _('Upssssss!'); ?></h1>
 
-        <p> <?php echo _('Sorry!! An error occurs. Try it again later!'); ?></p>
+        <p> <?php echo _('Sorry!! An error occurred. Try it again later!'); ?></p>
     </div>
 @stop
