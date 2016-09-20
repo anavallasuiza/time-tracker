c86176d13c3bf8354aa04a8a6ec7ab4f0dc69272
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2016-09-20 09:11:15 +0200

    Added deployer

---
+++ deploy.php
@@ -0,0 +1,69 @@
+<?php
+
+require 'recipe/composer.php';
+
+server(env('DEPLOY_SERVER_NAME'), env('DEPLOY_SERVER_HOST'), env('DEPLOY_SERVER_PORT'))
+    ->user(env('DEPLOY_SERVER_USER'))
+    ->identityFile(env('DEPLOY_IDENTIFY_FILE_PUBLIC_KEY'), env('DEPLOY_IDENTIFY_FILE_PRIVATE_KEY'))
+    ->stage(env('DEPLOY_STAGE'))
+    ->env('deploy_path', env('DEPLOY_PATH'));
+
+set('shared_files', [
+    '.env',
+]);
+
+set('shared_dirs', [
+    'storage/app',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+    'storage/gettext',
+    'public/storage',
+]);
+
+set('writable_dirs', [
+    'public/storage',
+    'bootstrap/cache',
+    'storage'
+]);
+
+task('deploy:upload', function () {
+    $release = env('release_path');
+    $content = [
+        'app',
+        'bootstrap',
+        'cache.json',
+        'composer.json',
+        'composer.lock',
+        'config',
+        'database',
+        'public',
+        'artisan',
+        'storage'
+    ];
+    foreach ($content as $path) {
+        upload($path, $release.'/'.$path);
+    }
+    foreach (['lang', 'views'] as $path) {
+        upload('resources/'.$path, $release.'/resources/'.$path);
+    }
+});
+
+task('deploy:publish', function () {
+    $path = env('release_path');
+    run("php {$path}/artisan vendor:publish");
+    run("php {$path}/artisan migrate");
+});
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:upload',
+    'deploy:vendors',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:publish',
+    'cleanup',
+]);
